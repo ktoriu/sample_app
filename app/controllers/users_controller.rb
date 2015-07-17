e817d7802022d8f3a288f9849fefe1635c07e40c
ktoriu <vagrant@localhost.localdomain>
2015-07-17 11:55:20 +0900

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy]
   before_action :is_signed_in, only: [:new, :create]
 
   def index
-    # @users = User.order(:name).paginate(page: params[:page], per_page: 30)
+
     @users = User.paginate(page: params[:page], per_page: 30)
   end
 
@@ -48,6 +48,21 @@ class UsersController < ApplicationController
     flash[:success] = "#{user.name}は削除されました"
     redirect_to users_url
   end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
 
   private
 
